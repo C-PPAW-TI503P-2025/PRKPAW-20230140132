cc3cf26c3f87694fe64ae6001add2721692411dd
Yowandaradillaa <160616789+Yowandaradillaa@users.noreply.github.com>
2025-10-17 14:56:38 +0700

    Add files via upload

---
+++ prak3/20230140132-react-app/src/app.js
@@ -0,0 +1,34 @@
+const express = require('express');
+const fs = require('fs');
+const app = express();
+const PORT = 3000;
+
+// Middleware untuk parsing JSON
+app.use(express.json());
+
+// Middleware logging
+app.use((req, res, next) => {
+  const log = `${new Date().toISOString()} - ${req.method} ${req.url}`;
+  console.log(log);
+  fs.appendFileSync('server.log', log + '\n');
+  next();
+});
+
+// Import router buku
+//const bookRoutes = require('./books');
+//app.use('/api/books', bookRoutes);
+
+// Middleware 404 Not Found (taruh setelah semua route)
+app.use((req, res, next) => {
+  res.status(404).json({ message: '404 Not Found' });
+});
+
+// Global error handler (paling bawah)
+app.use((err, req, res, next) => {
+  console.error(err.stack);
+  res.status(500).json({ message: 'Something went wrong!' });
+});
+
+app.listen(PORT, () => {
+  console.log(`Server running at http://localhost:${PORT}`);
+});
---
+++ prak3/Tugas3-20230140132/Tugas3.md
@@ -0,0 +1,10 @@
+Yowanda Radilla A
+20230140132
+
+Tampilan Endpoint:
+
+![Tampilan Endpoint presensi/check-in](ss/presensicheck-in.png)
+![Tampilan Endpoint presensi/check-in 2x](ss/presensicheck-in2x.png)
+![Tampilan Endpoint presensi/check-out](ss/presensicheck-out.png)
+![Tampilan Endpoint presensi/check-out 2x](ss/presensicheck-out2x.png)
+![Tampilan Endpoint reports/daily](ss/reportsdaily.png)
